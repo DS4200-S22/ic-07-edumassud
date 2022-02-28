c09257e0fabd4e17f39bc14b3e434d5ba0ff6364
edumassud <58275179+edumassud@users.noreply.github.com>
2022-02-28 13:17:14 -0500

    Still kinda confused

--- js/scatterplot.js
+++ js/scatterplot.js
@@ -9,67 +9,69 @@ Modified: 12/08/21
 // Build your scatterplot in this file 
 
 // Set dimensions and margins for the plot 
-const width = 900; 
-const height = 450; 
-const margin = {left:50, right:50, bottom:50, top:50}; 
+// const width1 = 900; 
+// const height = 450; 
+// const margin = {left:50, right:50, bottom:50, top:50}; 
 
 // add an svg to build within using deminsions set above
-const svg1 = d3
+const svg = d3
   .select("#csv-scatter")
   .append("svg")
   .attr("width", width-margin.left-margin.right)
-  .attr("height", height - margin.top - margin.bottom)
-  .attr("viewBox", [0, 0, width, height]);
+  .attr("height", height - margin.top - margin.bottom);
+
+  const g = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 
-d3.csv("data/scatter.csv").then((data) => { 
+  // X LABEL
+  g.append("text")
+    .attr("class", "x-axis lable")
+    .attr("x", - (height / 2))
+    .attr("y", -50)
+    .attr("text-anchor", "middle")
+    .attr("transform", "rotate(-90)")
+    .text("Days");
 
-    let maxX = d3.max(data, (d) => { return d.day; }); 
-    console.log("Max x: " + maxX); 
+  // Y LABEL
+  g.append("text")
+    .attr("class", "y-axis lable")
+    .attr("x", width / 2)
+    .attr("y", height + 100)
+    .attr("text-anchor", "middle")
+    .text("Score");
 
-    // find max Y 
-    let maxY = d3.max(data, (d) => { return d.score; }); 
-    console.log("Max y: " + maxY); 
+  d3.csv("data/scatter.csv").then(data => {
 
-    // Now that we have our maxes we define scale functions that
-    // map our data values (domain for the scale function) to our
-    // pixel values (range for the scale function)
+    console.log(data);
+    const x = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.day)])
+        .range([0, width]);
 
-    let xScale = d3.scaleLinear() // linear scale because we have 
-                                    // linear data 
-                    .domain([0, maxX])  // inputs for the function
-                    .range([margin.left, width - margin.right]); 
-                    // ^ outputs for the function 
+    const y = d3.scaleLinear()
+      .domain([0, d3.max(data, d => d.score)])
+      .range([0, height]);
 
-    let yScale = d3.scaleLinear()
-                .domain([0, maxY])
-                .range([height - margin.bottom, margin.top]); 
+    const xAxisCall = d3.axisBottom(x);
+    g.append("g")
+      .attr("class", "x axis")
+      .attr("transform", `translate(0, ${height})`)
+      .call(xAxisCall)
+      .selectAll("text")
+          .attr("text-anchor", "end")
+          .attr("transform", "rotate(-40)");
 
-    svg1.append("g") // g is a "placeholder" svg
-        .attr("transform", `translate(0,${height - margin.bottom})`) 
-        // ^ moves axis to bottom of svg 
-        .call(d3.axisBottom(xScale)) // built in function for bottom
-                                    // axis given a scale function 
-            .attr("font-size", '20px'); // set font size
-    
-    // Add y axis to svg6 
-    svg1.append("g") // g is a "placeholder" svg
-        .attr("transform", `translate(${margin.left}, 0)`) 
-        // ^ move axis inside of left margin
-        .call(d3.axisLeft(yScale)) // built in function for left
-                                    // axis given a scale function 
-        .attr("font-size", '20px'); // set font size
+    const yAxisCall = d3.axisLeft(y);
+    g.append("g")
+        .attr("class", "y axis")
+        .call(yAxisCall);
 
-  // add our circles with styling 
-  svg1.selectAll("circle") 
-      .data(data)
-      .enter()  
+    g.selectAll("circle")
+      .data("g")
+      .enter()
       .append("circle")
-        .attr("cx", (d) => xScale(d.day)) // use xScale to return 
-                                        // pixel value for given
-                                        // datum 
-        .attr("cy", (d) => yScale(d.score)) // use yScale to return 
-                                        // pixel value for given
-                                        // datum 
-        .attr("r", 10);
-});
+        .attr("cx", (d, i) => x(d.day))
+        .attr("cy", (d) => y(d.score))
+        .attr("r", 5)
+        .attr("fill", "red");
+  });
